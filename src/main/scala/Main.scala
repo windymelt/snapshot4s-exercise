1a83fd2439882fab2c43b19b8574e7a056dc245e
windymelt <windymelt@3qe.us>
2024-11-16 01:26:06 +0900

    refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,3 @@
-import scodec.bits.ByteVector
 object Module {
   def lipsum =
     """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
@@ -24,6 +23,8 @@ object JsonModule {
 object BinaryModule {
   import scodec.Codec
   import scodec.bits.hex
+  import scodec.bits.ByteVector
+
   val StringBOMCodec = scodec.codecs.utf8
   def withBOM(s: String): ByteVector =
     hex"EF BB BF" ++ scodec.codecs.utf8.encode(s).require.bytes
